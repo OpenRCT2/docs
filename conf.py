ee1fad9eb7c160d1a1e5f0df2f2643b99a2467d0
Miso Zmiric (Mike Squinter) <miso.zmiric@gmail.com>
2015-11-07 00:36:53 +0100

    use RTD theme everywhere

--- conf.py
+++ conf.py
@@ -47,7 +47,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'OpenRCT2'
-copyright = u'2015, OpenRCT2, IntelOrca, duncans_pumpkin'
+copyright = u'2015, OpenRCT2'
 author = u'OpenRCT2, IntelOrca, duncans_pumpkin'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -108,7 +108,16 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+
+# on_rtd is whether we are on readthedocs.org, this line of code grabbed from docs.readthedocs.org
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+#html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
